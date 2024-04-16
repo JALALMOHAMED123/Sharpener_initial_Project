e2574904649e1f7fbf5510f1d8c758c012d0b327
Jalal Mohamed <jalalmohamed2702@gmail.com>
2024-04-16 22:58:09 +0530

    getElementById method used

--- demo.js
+++ demo.js
@@ -1,1 +1,15 @@
-console.log("I'm in Git hub");
+const heading=document.getElementById("main-heading");
+heading.textContent="Fruit World";
+
+heading.style.color="orange";
+
+const backcolor=document.getElementById("header");
+backcolor.style.backgroundColor="green";
+
+backcolor.style.borderBottom='3px solid orange';
+
+const bheading=document.getElementById("basket-heading");
+bheading.style.color="green";
+
+const thanks=document.getElementById("thanks");
+thanks.innerHTML="<p>Please visit us again</p>";
